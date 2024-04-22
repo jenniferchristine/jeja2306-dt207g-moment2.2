5a6eb7a48045527b61aeac7610e3a46c60c26029
jenniferchristine <jeja2306@student.miun.se>
2024-04-22 22:34:10 +0200

    added delete-function

--- src/js/main.js
+++ src/js/main.js
@@ -28,17 +28,42 @@ async function displayData() {
             <h2>Arbetsplats: ${item.companyname}</h2>
             <h3>Jobtitel: ${item.jobtitle}</h3>
             <h4>Ort: ${item.location}</h4>
-            <p><span class="desc">"</span> ${item.description} <span class="desc">"</span></p>
-            `;
+            <p><span class="desc">"</span> ${item.description} <span class="desc">"</span></p>`;
+
             const deleteBtn = document.createElement("button");
             deleteBtn.classList.add("material-symbols-outlined");
             deleteBtn.innerHTML = "delete";
             workExperience.appendChild(deleteBtn);
             resultDiv.appendChild(workExperience);
+
+            deleteBtn.addEventListener('click', () => {
+                resultDiv.removeChild(workExperience);
+                deleteData(item.id);
+            });
         });
     } catch (error) {
         console.error("Fault accured: ", error);
     }
+};
+
+function deleteData(id) {
+    const url = "https://jeja2306-dt207g-moment2-1.onrender.com/cv/workexperience/" + id;
+
+    fetch(url, {
+        method: 'DELETE'
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Failed to delete data");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("Data deleted", data);
+        })
+        .catch(error => {
+            console.error("Error when deleting data", error);
+        });
 };
 
 displayData(); // hämtar och visar vid start av sida
